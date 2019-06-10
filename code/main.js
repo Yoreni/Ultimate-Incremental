abc91bdc8db43226abd30c8e854669ac4f5db68f
theMkeyholder <51534449+theMkeyholder@users.noreply.github.com>
2019-06-10 18:54:53 -0400

    Update main.js

--- code/main.js
+++ code/main.js
@@ -74,58 +74,9 @@ function clickPoints()
 var app = new Vue({
   el: '#app2',
   data: {
-    message: 'Hello Vue!' + points
+    message: points
   }
 })
-/*
-var app2 = new Vue({
-  el: '#app-2',
-  data: {
-    message: 'You loaded this page on ' + new Date().toLocaleString()
-  }
-})
-
-var app3 = new Vue({
-  el: '#app-3',
-  data: {
-    seen: false
-  }
-})
-
-var app4 = new Vue({
-  el: '#app-4',
-  data: {
-    todos: [
-      { text: 'Learn JavaScript' },
-      { text: 'Learn Vue' },
-      { text: 'Build something awesome' }
-    ]
-  }
-})
-
-var app5 = new Vue({
-  el: '#app-5',
-  data: {
-    message: 'Hello Vue.js!'
-  },
-  methods: {
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
-    }
-  }
-})
-
-var app6 = new Vue({
-  el: '#app-6',
-  data: {
-    message: 'Hello Vue!'
-  }
-})
-
-Vue.component('todo-item', {
-  props: ['todo'],
-  template: '<li>{{ todo.text }}</li>'
-})*/
 
 function gameLoop()
 {
